1d92c39c43993cccaee6b49e198c1c297bf5c111
Kirill Isakov <bootctl@gmail.com>
2022-03-31 21:19:59 +0600

    Adjust CI to new integration tests

--- .ci/windows/build.cmd
+++ .ci/windows/build.cmd
@@ -1,8 +1,13 @@
 set crypto=%1
 set builddir=%crypto%
+set args=
+
+if %TYPE% == cross (
+    set args=--cross-file .ci/cross/msvc/%ARCH:amd64_=%
+)
 
 echo configure build directory
-meson setup %builddir% -Dbuildtype=release -Dcrypto=%crypto% || exit 1
+meson setup %builddir% -Dbuildtype=release -Dcrypto=%crypto% %args% || exit 1
 
 echo build project
 meson compile -C %builddir% || exit 1
